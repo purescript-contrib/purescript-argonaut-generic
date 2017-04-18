90cc5316d3c3413c89a7641034c4550c6d1d44b1
FrigoEU <simon.van.casteren@gmail.com>
2017-04-18 13:58:34 +0200

    Initial release: copy from purescript-argonaut-codecs

---
+++ README.md
@@ -0,0 +1,16 @@
+# purescript-argonaut-generic
+
+[![Latest release](http://img.shields.io/github/release/purescript-contrib/purescript-dom.svg)](https://github.com/purescript-contrib/purescript-argonaut-generic/releases)
+[![Build status](https://travis-ci.org/purescript-contrib/purescript-argonaut-generic.svg?branch=master)](https://travis-ci.org/purescript-contrib/purescript-argonaut-generic)
+[![Maintainer: slamdata](https://img.shields.io/badge/maintainer-slamdata-lightgrey.svg)](http://github.com/slamdata)
+
+This package provides encodeJson and decodeJson functions for any data type that has a Generic instance. Currently only purescript-generics is supported (not purescript-generics-rep).
+
+## Installation
+
+```shell
+bower install purescript-argonaut-generic
+```
+
+## Documentation
+
---
+++ src/Data/Argonaut/Encode/Generic.purs
@@ -0,0 +1,35 @@
+module Data.Argonaut.Encode.Generic where
+
+import Prelude
+
+import Data.Argonaut.Encode.Class (encodeJson)
+import Data.Argonaut.Core (Json(), jsonNull, fromBoolean, fromNumber, fromString, fromArray, fromObject)
+import Data.Int (toNumber)
+import Data.Foldable (foldr)
+import Data.Generic (class Generic, GenericSpine(..), toSpine)
+import Data.String (singleton)
+import Data.StrMap as SM
+
+-- | Encode any `Generic` data structure into `Json`.
+gEncodeJson :: forall a. Generic a => a -> Json
+gEncodeJson = gEncodeJson' <<< toSpine
+
+-- | Encode `GenericSpine` into `Json`.
+gEncodeJson' :: GenericSpine -> Json
+gEncodeJson' = case _ of
+  SInt x -> fromNumber $ toNumber x
+  SString x -> fromString x
+  SChar x -> fromString $ singleton x
+  SNumber x -> fromNumber x
+  SBoolean x -> fromBoolean x
+  SArray thunks -> fromArray (gEncodeJson' <<< (unit # _) <$> thunks)
+  SUnit -> jsonNull
+  SProd constr args ->
+    fromObject
+      $ SM.insert "tag" (encodeJson constr)
+      $ SM.singleton "values" (encodeJson (gEncodeJson' <<< (unit # _) <$> args))
+  SRecord fields ->
+    fromObject $ foldr addField SM.empty fields
+    where
+    addField field =
+      SM.insert field.recLabel (gEncodeJson' $ field.recValue unit)

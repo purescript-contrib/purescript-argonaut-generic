2b16a32be2779f7aa42b7081ce4c855c6e4322ee
rightfold <rightfold@gmail.com>
2017-06-05 13:03:20 +0200

    Add generics-rep encoding and decoding

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 [![Build status](https://travis-ci.org/purescript-contrib/purescript-argonaut-generic.svg?branch=master)](https://travis-ci.org/purescript-contrib/purescript-argonaut-generic)
 [![Maintainer: slamdata](https://img.shields.io/badge/maintainer-slamdata-lightgrey.svg)](http://github.com/slamdata)
 
-This package provides `gEncodeJson` and `gDecodeJson` functions for any data types that have a `Generic` instance. Currently only [`purescript-generics`](https://github.com/purescript/purescript-generics) is supported (not [`purescript-generics-rep`](https://github.com/purescript/purescript-generics-rep)).
+This package provides `gEncodeJson` and `gDecodeJson` functions for any data types that have a `Generic` instance, and `genericEncodeJson` and `genericDecodeJson` functions for any data types that have a `Rep.Generic` instance.
 
 ## Installation
 
--- src/Data/Argonaut/Encode/Generic.purs
+++ src/Data/Argonaut/Encode/Generic.purs
@@ -1,17 +1,27 @@
 module Data.Argonaut.Encode.Generic (
-  gEncodeJson, 
-  gEncodeJson'
+  gEncodeJson,
+  gEncodeJson',
+
+  class EncodeRep,
+  class EncodeRepArgs,
+  class EncodeRepFields,
+  encodeRep,
+  encodeRepArgs,
+  encodeRepFields,
+  genericEncodeJson
 ) where
 
 import Prelude
 
-import Data.Argonaut.Encode.Class (encodeJson)
+import Data.Argonaut.Encode.Class (class EncodeJson, encodeJson)
 import Data.Argonaut.Core (Json(), jsonNull, fromBoolean, fromNumber, fromString, fromArray, fromObject)
 import Data.Int (toNumber)
 import Data.Foldable (foldr)
 import Data.Generic (class Generic, GenericSpine(..), toSpine)
+import Data.Generic.Rep as Rep
 import Data.String (singleton)
 import Data.StrMap as SM
+import Data.Symbol (class IsSymbol, SProxy(..), reflectSymbol)
 
 -- | Encode any `Generic` data structure into `Json`.
 gEncodeJson :: forall a. Generic a => a -> Json
@@ -36,3 +46,50 @@ gEncodeJson' = case _ of
     where
     addField field =
       SM.insert field.recLabel (gEncodeJson' $ field.recValue unit)
+
+class EncodeRep r where
+  encodeRep :: r -> Json
+
+instance encodeRepNoConstructors :: EncodeRep Rep.NoConstructors where
+  encodeRep r = encodeRep r
+
+instance encodeRepSum :: (EncodeRep a, EncodeRep b) => EncodeRep (Rep.Sum a b) where
+  encodeRep (Rep.Inl a) = encodeRep a
+  encodeRep (Rep.Inr b) = encodeRep b
+
+instance encodeRepConstructor :: (IsSymbol name, EncodeRepArgs a) => EncodeRep (Rep.Constructor name a) where
+  encodeRep (Rep.Constructor a) =
+    fromObject
+      $ SM.insert "tag" (fromString (reflectSymbol (SProxy :: SProxy name)))
+      $ SM.insert "values" (fromArray (encodeRepArgs a))
+      $ SM.empty
+
+class EncodeRepArgs r where
+  encodeRepArgs :: r -> Array Json
+
+instance encodeRepArgsNoArguments :: EncodeRepArgs Rep.NoArguments where
+  encodeRepArgs Rep.NoArguments = []
+
+instance encodeRepArgsProduct :: (EncodeRepArgs a, EncodeRepArgs b) => EncodeRepArgs (Rep.Product a b) where
+  encodeRepArgs (Rep.Product a b) = encodeRepArgs a <> encodeRepArgs b
+
+instance encodeRepArgsArgument :: (EncodeJson a) => EncodeRepArgs (Rep.Argument a) where
+  encodeRepArgs (Rep.Argument a) = [encodeJson a]
+
+instance encodeRepArgsRec :: (EncodeRepFields fields) => EncodeRepArgs (Rep.Rec fields) where
+  encodeRepArgs (Rep.Rec fields) = [fromObject $ encodeRepFields fields]
+
+class EncodeRepFields r where
+  encodeRepFields :: r -> SM.StrMap Json
+
+instance encodeRepFieldsProduct :: (EncodeRepFields a, EncodeRepFields b) => EncodeRepFields (Rep.Product a b) where
+  encodeRepFields (Rep.Product a b) =
+    SM.union (encodeRepFields a) (encodeRepFields b)
+
+instance encodeRepFieldsField :: (IsSymbol field, EncodeJson a) => EncodeRepFields (Rep.Field field a) where
+  encodeRepFields (Rep.Field a) =
+    SM.singleton (reflectSymbol (SProxy :: SProxy field))
+                 (encodeJson a)
+
+genericEncodeJson :: forall a r. Rep.Generic a r => EncodeRep r => a -> Json
+genericEncodeJson = encodeRep <<< Rep.from

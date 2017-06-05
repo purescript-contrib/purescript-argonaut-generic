7550a6bf53b5857d3cfb61c021eb822cf9445571
rightfold <rightfold@gmail.com>
2017-06-05 14:08:08 +0200

    Move generics-rep code to separate module

--- src/Data/Argonaut/Decode/Generic.purs
+++ src/Data/Argonaut/Decode/Generic.purs
@@ -1,32 +1,19 @@
 module Data.Argonaut.Decode.Generic (
   gDecodeJson,
-  gDecodeJson',
-
-  class DecodeRep,
-  class DecodeRepArgs,
-  class DecodeRepFields,
-  decodeRep,
-  decodeRepArgs,
-  decodeRepFields,
-  genericDecodeJson
+  gDecodeJson'
 ) where
 
 import Prelude
 
-import Control.Alt ((<|>))
 import Data.Argonaut.Core (Json, toArray, toBoolean, toNumber, toObject, toString)
-import Data.Argonaut.Decode.Class (class DecodeJson, decodeJson)
-import Data.Array (uncons, zipWithA)
-import Data.Bifunctor (lmap)
+import Data.Array (zipWithA)
 import Data.Either (Either(..))
 import Data.Foldable (find)
 import Data.Generic (class Generic, GenericSpine(..), GenericSignature(..), fromSpine, toSignature)
-import Data.Generic.Rep as Rep
 import Data.Int (fromNumber)
 import Data.Maybe (maybe, Maybe(..))
 import Data.String (toChar)
 import Data.StrMap as SM
-import Data.Symbol (class IsSymbol, SProxy(..), reflectSymbol)
 import Data.Traversable (traverse, for)
 import Type.Proxy (Proxy(..))
 
@@ -66,69 +53,6 @@ gDecodeJson' signature json = case signature of
         vals <- mFail (decodingErr "'values' array is missing") (toArray =<< SM.lookup "values" jObj)
         sps  <- zipWithA (\k -> gDecodeJson' (k unit)) sigValues vals
         pure (SProd tag (const <$> sps))
-
-class DecodeRep r where
-  decodeRep :: Json -> Either String r
-
-instance decodeRepNoConstructors :: DecodeRep Rep.NoConstructors where
-  decodeRep _ = Left "Cannot decode empty data type"
-
-instance decodeRepSum :: (DecodeRep a, DecodeRep b) => DecodeRep (Rep.Sum a b) where
-  decodeRep j = Rep.Inl <$> decodeRep j <|> Rep.Inr <$> decodeRep j
-
-instance decodeRepConstructor :: (IsSymbol name, DecodeRepArgs a) => DecodeRep (Rep.Constructor name a) where
-  decodeRep j = do
-    let name = reflectSymbol (SProxy :: SProxy name)
-    let decodingErr msg = "When decoding a " <> name <> ": " <> msg
-    jObj <- mFail (decodingErr "expected an object") (toObject j)
-    jTag <- mFail (decodingErr "'tag' property is missing") (SM.lookup "tag" jObj)
-    tag <- mFail (decodingErr "'tag' property is not a string") (toString jTag)
-    when (tag /= name) $
-      Left $ decodingErr "'tag' property has an incorrect value"
-    jValues <- mFail (decodingErr "'values' property is missing") (SM.lookup "values" jObj)
-    values <- mFail (decodingErr "'values' property is not an array") (toArray jValues)
-    {init, rest} <- lmap decodingErr $ decodeRepArgs values
-    when (rest /= []) $
-      Left $ decodingErr "'values' property had too many values"
-    pure $ Rep.Constructor init
-
-class DecodeRepArgs r where
-  decodeRepArgs :: Array Json -> Either String {init :: r, rest :: Array Json}
-
-instance decodeRepArgsNoArguments :: DecodeRepArgs Rep.NoArguments where
-  decodeRepArgs js = Right {init: Rep.NoArguments, rest: js}
-
-instance decodeRepArgsProduct :: (DecodeRepArgs a, DecodeRepArgs b) => DecodeRepArgs (Rep.Product a b) where
-  decodeRepArgs js = do
-    {init: a, rest: js'} <- decodeRepArgs js
-    {init: b, rest: js''} <- decodeRepArgs js'
-    pure {init: Rep.Product a b, rest: js''}
-
-instance decodeRepArgsArgument :: (DecodeJson a) => DecodeRepArgs (Rep.Argument a) where
-  decodeRepArgs js = do
-    {head, tail} <- mFail "too few values were present" (uncons js)
-    {init: _, rest: tail} <<< Rep.Argument <$> decodeJson head
-
-instance decodeRepArgsRec :: (DecodeRepFields fields) => DecodeRepArgs (Rep.Rec fields) where
-  decodeRepArgs js = do
-    {head, tail} <- mFail "too few values were present" (uncons js)
-    jObj <- mFail "record is not encoded as an object" (toObject head)
-    {init: _, rest: tail} <<< Rep.Rec <$> decodeRepFields jObj
-
-class DecodeRepFields r where
-  decodeRepFields :: SM.StrMap Json -> Either String r
-
-instance decodeRepFieldsProduct :: (DecodeRepFields a, DecodeRepFields b) => DecodeRepFields (Rep.Product a b) where
-  decodeRepFields js = Rep.Product <$> decodeRepFields js <*> decodeRepFields js
-
-instance decodeRepFieldsField :: (IsSymbol field, DecodeJson a) => DecodeRepFields (Rep.Field field a) where
-  decodeRepFields js = do
-    let name = reflectSymbol (SProxy :: SProxy field)
-    value <- mFail ("the field '" <> name <> "' is not present") (SM.lookup name js)
-    Rep.Field <$> decodeJson value
-
-genericDecodeJson :: forall a r. Rep.Generic a r => DecodeRep r => Json -> Either String a
-genericDecodeJson = map Rep.to <<< decodeRep
-
-mFail :: forall a. String -> Maybe a -> Either String a
-mFail msg = maybe (Left msg) Right
+  where
+  mFail :: forall a. String -> Maybe a -> Either String a
+  mFail msg = maybe (Left msg) Right
---
+++ src/Data/Argonaut/Encode/Generic/Rep.purs
@@ -0,0 +1,64 @@
+module Data.Argonaut.Encode.Generic.Rep (
+  class EncodeRep,
+  class EncodeRepArgs,
+  class EncodeRepFields,
+  encodeRep,
+  encodeRepArgs,
+  encodeRepFields,
+  genericEncodeJson
+) where
+
+import Prelude
+
+import Data.Argonaut.Encode.Class (class EncodeJson, encodeJson)
+import Data.Argonaut.Core (Json, fromArray, fromObject, fromString)
+import Data.Generic.Rep as Rep
+import Data.StrMap as SM
+import Data.Symbol (class IsSymbol, SProxy(..), reflectSymbol)
+
+class EncodeRep r where
+  encodeRep :: r -> Json
+
+instance encodeRepNoConstructors :: EncodeRep Rep.NoConstructors where
+  encodeRep r = encodeRep r
+
+instance encodeRepSum :: (EncodeRep a, EncodeRep b) => EncodeRep (Rep.Sum a b) where
+  encodeRep (Rep.Inl a) = encodeRep a
+  encodeRep (Rep.Inr b) = encodeRep b
+
+instance encodeRepConstructor :: (IsSymbol name, EncodeRepArgs a) => EncodeRep (Rep.Constructor name a) where
+  encodeRep (Rep.Constructor a) =
+    fromObject
+      $ SM.insert "tag" (fromString (reflectSymbol (SProxy :: SProxy name)))
+      $ SM.insert "values" (fromArray (encodeRepArgs a))
+      $ SM.empty
+
+class EncodeRepArgs r where
+  encodeRepArgs :: r -> Array Json
+
+instance encodeRepArgsNoArguments :: EncodeRepArgs Rep.NoArguments where
+  encodeRepArgs Rep.NoArguments = []
+
+instance encodeRepArgsProduct :: (EncodeRepArgs a, EncodeRepArgs b) => EncodeRepArgs (Rep.Product a b) where
+  encodeRepArgs (Rep.Product a b) = encodeRepArgs a <> encodeRepArgs b
+
+instance encodeRepArgsArgument :: (EncodeJson a) => EncodeRepArgs (Rep.Argument a) where
+  encodeRepArgs (Rep.Argument a) = [encodeJson a]
+
+instance encodeRepArgsRec :: (EncodeRepFields fields) => EncodeRepArgs (Rep.Rec fields) where
+  encodeRepArgs (Rep.Rec fields) = [fromObject $ encodeRepFields fields]
+
+class EncodeRepFields r where
+  encodeRepFields :: r -> SM.StrMap Json
+
+instance encodeRepFieldsProduct :: (EncodeRepFields a, EncodeRepFields b) => EncodeRepFields (Rep.Product a b) where
+  encodeRepFields (Rep.Product a b) =
+    SM.union (encodeRepFields a) (encodeRepFields b)
+
+instance encodeRepFieldsField :: (IsSymbol field, EncodeJson a) => EncodeRepFields (Rep.Field field a) where
+  encodeRepFields (Rep.Field a) =
+    SM.singleton (reflectSymbol (SProxy :: SProxy field))
+                 (encodeJson a)
+
+genericEncodeJson :: forall a r. Rep.Generic a r => EncodeRep r => a -> Json
+genericEncodeJson = encodeRep <<< Rep.from

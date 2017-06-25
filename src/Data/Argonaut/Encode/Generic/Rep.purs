926213b828a00bb5b4ca99c4a0d2b0d845c3de87
justinwoo <moomoowoo@gmail.com>
2017-06-25 19:24:28 +0300

    add encoding of string literals to sum types of nullary constructors

--- src/Data/Argonaut/Encode/Generic/Rep.purs
+++ src/Data/Argonaut/Encode/Generic/Rep.purs
@@ -2,19 +2,24 @@ module Data.Argonaut.Encode.Generic.Rep (
   class EncodeRep,
   class EncodeRepArgs,
   class EncodeRepFields,
+  class EncodeLiteral,
   encodeRep,
   encodeRepArgs,
   encodeRepFields,
-  genericEncodeJson
+  genericEncodeJson,
+  encodeLiteralSum,
+  encodeLiteralSumWithTransform,
+  encodeLiteral
 ) where
 
 import Prelude
 
-import Data.Argonaut.Encode.Class (class EncodeJson, encodeJson)
 import Data.Argonaut.Core (Json, fromArray, fromObject, fromString)
+import Data.Argonaut.Encode.Class (class EncodeJson, encodeJson)
 import Data.Generic.Rep as Rep
 import Data.StrMap as SM
 import Data.Symbol (class IsSymbol, SProxy(..), reflectSymbol)
+import Partial.Unsafe (unsafeCrashWith)
 
 class EncodeRep r where
   encodeRep :: r -> Json
@@ -63,3 +68,29 @@ instance encodeRepFieldsField :: (IsSymbol field, EncodeJson a) => EncodeRepFiel
 -- | Encode any `Generic` data structure into `Json`.
 genericEncodeJson :: forall a r. Rep.Generic a r => EncodeRep r => a -> Json
 genericEncodeJson = encodeRep <<< Rep.from
+
+-- | A function for encoding `Generic` sum types using string literal representations
+encodeLiteralSum :: forall a r. Rep.Generic a r => EncodeLiteral r => a -> Json
+encodeLiteralSum = encodeLiteralSumWithTransform id
+
+-- | A function for encoding `Generic` sum types using string literal representations
+-- | Takes a function for transforming the tag name in encoding
+encodeLiteralSumWithTransform :: forall a r. Rep.Generic a r => EncodeLiteral r => (String -> String) -> a -> Json
+encodeLiteralSumWithTransform tagNameTransform = encodeLiteral tagNameTransform <<< Rep.from
+
+class EncodeLiteral r where
+  encodeLiteral :: (String -> String) -> r -> Json
+
+instance encodeLiteralSumInst :: (EncodeLiteral a, EncodeLiteral b) => EncodeLiteral (Rep.Sum a b) where
+  encodeLiteral tagNameTransform (Rep.Inl a) = encodeLiteral tagNameTransform a
+  encodeLiteral tagNameTransform (Rep.Inr b) = encodeLiteral tagNameTransform b
+
+instance encodeLiteralConstructor :: (IsSymbol name) => EncodeLiteral (Rep.Constructor name (Rep.NoArguments)) where
+  encodeLiteral tagNameTransform _ = fromString <<< tagNameTransform $ reflectSymbol (SProxy :: SProxy name)
+
+type FailMessage = """`encodeLiteralSum` can only be used with sum types, where all of the constructors are nullary. This is because a string literal cannot be encoded into a product type."""
+
+instance encodeLiteralConstructorCannotBeProduct
+  :: Fail FailMessage
+  => EncodeLiteral (Rep.Product a b) where
+  encodeLiteral _ _ = unsafeCrashWith "unreachable encodeLiteral was reached."
